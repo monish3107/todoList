4f3834e01dfe16539ebdb6bd15048efdac8ae04e
monish3107 <112330264+monish3107@users.noreply.github.com>
2024-12-29 22:12:41 +0530

    Update urls.py

--- todoList/urls.py
+++ todoList/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
     path('tasks/', views.tasks, name='tasks'),  # Ensure this is defined
 
 ]
