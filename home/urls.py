d0ff78856c9ae0126c72d3e42635feb426175656
monish3107 <112330264+monish3107@users.noreply.github.com>
2024-12-30 09:28:53 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('task/', views.task, name='tasks'),
-    path('task/toggle/<int:task_id>/', views.toggle_task_completion, name='toggle_task_completion'),  
+    path('', views.home, name='home'),  
 ]
