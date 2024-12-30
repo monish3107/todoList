8a765acadb447a8ed6437a5c77da73308ff13351
monish3107 <112330264+monish3107@users.noreply.github.com>
2024-12-30 09:27:13 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -5,18 +5,27 @@ from home.models import Task
 def home(request):
 
     if request.method == "POST":
+        
         if "title" in request.POST:
             title = request.POST.get("title")
             desc = request.POST.get("desc")
             ins = Task(taskTitle=title, taskDesc=desc)
             ins.save()
             context = {'success': True}
-        elif "task_id" in request.POST: 
+            
+        elif "task_id" in request.POST:  
             task_id = request.POST.get("task_id")
             task = get_object_or_404(Task, id=task_id)
             task.completed = not task.completed
             task.save()
-            context = {'success': False}  
+            
+            
+        elif "delete_task_id" in request.POST:  
+            delete_task_id = request.POST.get("delete_task_id")
+            task = get_object_or_404(Task, id=delete_task_id)
+            task.delete()
+            context = {'success': True, 'message': 'Task deleted successfully!'}
+            
         return redirect('/')
 
     search_query = request.GET.get('search', '')
