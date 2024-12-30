3dadeb39c99ee46ef95fa69200da974a878c2002
monish3107 <112330264+monish3107@users.noreply.github.com>
2024-12-30 09:28:27 +0530

    Update urls.py

--- todoList/urls.py
+++ todoList/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),  
 ]
