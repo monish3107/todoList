3e0346fb69f985fec3a3e19ff6b203b69aef1592
monish3107 <112330264+monish3107@users.noreply.github.com>
2025-01-06 14:07:52 +0530

    Update wsgi.py

--- todoList/wsgi.py
+++ todoList/wsgi.py
@@ -1,18 +1,15 @@
-"""
-WSGI config for todoList project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoList.settings')
 
+# Ensure the app uses the correct port when running on Render.
+port = os.getenv("PORT", "8000")
+os.environ["PORT"] = port
+
+# Get the WSGI application for Django.
 application = get_wsgi_application()
 
+# Alias it to 'app' for Render to recognize it.
 app = application
