a5d763e621be362d18a0de007a600f21e18643a1
monish3107 <112330264+monish3107@users.noreply.github.com>
2025-01-06 14:00:01 +0530

    Update settings.py

--- todoList/settings.py
+++ todoList/settings.py
@@ -15,8 +15,7 @@ SECRET_KEY = 'django-insecure-4r4)3(nkb8dioe7=(sd$e*sg$37_n0@r14jd5krnsml(m!skv+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 # Application definition
 
 INSTALLED_APPS = [
